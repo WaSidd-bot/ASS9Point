11a30908af69a117e3c578ce021d430305abcef0
Wasay Siddiqui <wasaysiddiqui379@gmail.com>
2025-04-14 09:59:31 -0400

    figuring out logic for adding points together

--- src/Point.java
+++ src/Point.java
@@ -1,4 +1,21 @@
 public class Point {
 
+    private double x = 0;
+    private double y = 0;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+    public double GetX() {
+        return x;
+    }
+
+    public double GetY() {
+        return y;
+    }
+
     
-}
+}   
